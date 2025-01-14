6b02d0c57861c36b7a6902e629535a9bc67eadd7
Nadia Trabelsi <megdiche.nadia1996@gmail.com>
2025-01-14 13:54:19 +0100

    index.php

--- controllers/AcceuilController.php
+++ controllers/AcceuilController.php
@@ -1,14 +1,13 @@
-<?php 
+<?php
 
+require_once __DIR__ . '/../models/Article.php';
 
-Class AcceuilController extends Controller{
+class AcceuilController extends Controller {
 
-    public function index(){
-
-        $this->render('index');
-
+    public function index() {
+        $article = new Article();
+        $articles = $article->getAll(); // Récupère tous les articles
+        $this->render('index', ['articles' => $articles]);
     }
 }
-
-
 ?>
